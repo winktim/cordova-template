cf9ebbf85aa19d513c94c05555a09871674ca4c0
= <scottlandart@gmail.com>
2018-01-16 23:58:02 +0100

    modification meta

--- meta.js
+++ meta.js
@@ -1,41 +1,32 @@
 module.exports = {
-  "helpers": {
-    "if_or": function (v1, v2, options) {
-      if (v1 || v2) {
-        return options.fn(this);
-      }
-
-      return options.inverse(this);
-    }
-  },
-  "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "message": "Project name (all lowercase)"
+    "prompts": {
+        "name": {
+            "type": "string",
+            "required": true,
+            "message": "Project name (all lowercase)"
+        },
+        "package": {
+            "type": "string",
+            "required": true,
+            "message": "Package name (ex: io.cordova.helloworld)"
+        },
+        "description": {
+            "type": "string",
+            "required": false,
+            "message": "Project description",
+            "default": "Cordova project"
+        },
+        "author": {
+            "type": "string",
+            "message": "Author"
+        },
+        "vuex": {
+            "type": "confirm",
+            "message": "Install Vuex for state management?"
+        }
     },
-    "package": {
-      "type": "string",
-      "required": true,
-      "message": "Package name (ex: io.cordova.helloworld)"
+    "filters": {
+        "src/store.js": "vuex"
     },
-    "description": {
-      "type": "string",
-      "required": false,
-      "message": "Project description",
-      "default": "Cordova project"
-    },
-    "author": {
-      "type": "string",
-      "message": "Author"
-    },
-    "vuex": {
-      "type": "confirm",
-      "message": "Install Vuex for state management?"
-    }
-  },
-  "filters": {
-    "src/store.js": "vuex"
-  },
-  "completeMessage": "Run\n\n cd {{destDirName}}\n{{/inPlace}}npm install\n"
+    "completeMessage": "Run\n\ncd {{destDirName}}\nnpm install\nnpm run build"
 }
